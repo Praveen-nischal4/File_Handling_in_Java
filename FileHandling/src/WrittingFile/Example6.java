55fc9e600464e11f040ecf8b904861ef82e7a490
PRAVEEN <PRAVEEN@DESKTOP-T94BTI2>
2024-02-07 01:11:02 +0000

    Program 6

---
+++ FileHandling/src/WrittingFile/Example6.java
@@ -0,0 +1,35 @@
+//You can also write Data in file can also be written using DataOutputStream .
+
+package WrittingFile;
+import java.io.*;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		DataOutputStream out = null;
+		try
+		{
+     FileOutputStream fo =new FileOutputStream("tiger.txt");
+     out = new DataOutputStream(fo);
+     out.writeInt(256);
+     out.flush();
+     System.out.println("Data written successfully");
+		}catch(IOException e)
+		{
+			System.out.println(e);
+		}finally {
+			if(out!=null)
+			{
+				try
+				{
+					out.close();
+				}catch(IOException f)
+				{
+					System.out.println(f);
+				}
+			}
+		}
+		
+	}
+
+}
